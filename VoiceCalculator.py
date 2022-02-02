5be6d24d2b231934cec434d987534cb3fc23d1a1
Sandeep <sk7503035188@gmail.com>
2022-02-02 17:02:01 +0530

    Voice calculator

---
+++ VoiceCalculator.py
@@ -0,0 +1,43 @@
+import pyttsx3
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+speak("Enter your Number 1")
+num1 = int(input("Enter your Number 1: "))
+speak("Enter your Number 2")
+num2 = int(input("Enter your Number 2: "))
+speak(f"Your numbers is {num1} and {num2} is: ")
+
+print("for Add press 1:")
+print("for Substraction press 2:")
+print("for multiplication press 3:")
+print("for division press 4:")
+
+speak(" for Add press 1:, for Substraction press 2:,  for multiplication press 3:, for division press 4:")
+num7 = int(input("Enter your fraction Number "))
+
+if num7 == 1:
+    add = num1+num2
+    print(" Your answer is......", add)
+    speak(f" Your answer is......{add}")
+elif num7 == 2:
+    sub = num1-num2
+    print(" Your answer is......", sub)
+    speak(f" Your answer is......{sub}")
+elif num7 == 3:
+    mul = num1*num2
+    print(" Your answer is......", mul)
+    speak(f" Your answer is......{mul}")
+elif num7 == 4:
+    div = num1/num2
+    print(" Your answer is......", div)
+    speak(f" Your answer is......{div}")
